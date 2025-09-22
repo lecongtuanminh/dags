96a9e8a51aea0567abab9760b48a88b271c8e15a
lecongtuanminh <82893280+lecongtuanminh@users.noreply.github.com>
2025-09-22 17:44:04 +0700

    Update dag_1.py

--- dag_1.py
+++ dag_1.py
@@ -3,10 +3,21 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-def list_buckets():
+def check_buckets():
     hook = S3Hook(aws_conn_id="minio_conn")
-    print(hook.list_buckets())
+    # boto3 client
+    client = hook.get_conn()
+    response = client.list_buckets()
+    for b in response.get("Buckets", []):
+        print(f"Bucket: {b['Name']}")
 
-with DAG("test_s3", start_date=datetime(2023, 1, 1), schedule="@once", catchup=False) as dag:
-    PythonOperator(task_id="list_buckets", python_callable=list_buckets)
-
+with DAG(
+    "test_minio_connection",
+    start_date=datetime(2023, 1, 1),
+    schedule="@once",
+    catchup=False,
+) as dag:
+    PythonOperator(
+        task_id="list_minio_buckets",
+        python_callable=check_buckets,
+    )
